4cdbb25ea4d253d720460ff4e1f371fad6ff42cc
Phoenix.S <PhoenixSong@Outlook.com>
2017-12-06 17:55:05 -0800

    Cleaned up app.js.

--- app.js
+++ app.js
@@ -5,25 +5,16 @@
  * This software is released under the MIT License.
  * https://opensource.org/licenses/MIT
  */
-const https = require('https'); // Built-in https module.
-const fs = require('fs'); // Built-in filesystem module.
-const Koa = require('koa'); // koa.js server framework.
-const mount = require('koa-mount'); // A router middleware for koa.
-const KoaLogger = require('koa-logger'); // Logger middleware for koa.
-const request = require('request'); // A convenient http request package.
+const https = require('https');
+const fs = require('fs');
+const Koa = require('koa');
+const mount = require('koa-mount');
+const KoaLogger = require('koa-logger');
+const request = require('request');
 
-// A static configuration that can be extracted into json config file.
-const CONFIGS = [
-  {
-    prefix: '/locationiq',
-    baseUrl: 'https://locationiq.org',
-    query: { key: '93a4e4e1cf3f0c', format: 'json' },
-    headers: {}
-  }
-];
-// Initilize the Koa app instance with logger middleware;
+const CONFIGS = JSON.parse(fs.readFileSync('config.json'));
+
 const app = new Koa().use(KoaLogger());
-// Iterate and mount proxy applications specified in CONFIGS.
 for (const { prefix, baseUrl, query = {}, headers = {} } of CONFIGS) {
   app.use(
     mount(
@@ -34,28 +25,26 @@ for (const { prefix, baseUrl, query = {}, headers = {} } of CONFIGS) {
           method: ctx.method,
           uri: ctx.path,
           headers: { ...ctx.headers, ...headers },
-          qs: { ...ctx.query, ...query }
+          qs: { ...ctx.query, ...query },
         };
         if (remoteReqOption.headers.host) delete remoteReqOption.headers.host;
         ctx.body = ctx.req.pipe(request(remoteReqOption)).once('response', resp => {
-          // Setting the headers
           ctx.response.status = resp.statusCode;
           ctx.response.message = resp.statusMessage;
           ctx.response.set(resp.headers);
         });
-      })
-    )
+      }),
+    ),
   );
 }
 
-// Initialize a https server.
 https
   .createServer(
     {
       cert: fs.readFileSync('cert/server.crt'),
-      key: fs.readFileSync('cert/server.key')
+      key: fs.readFileSync('cert/server.key'),
     },
-    app.callback() // Use the Koa app as requests listener.
+    app.callback(),
   )
   .listen(3000);
 
